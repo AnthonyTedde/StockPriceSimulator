bd8651c7657fac530479f9a555e78cdc058c9ef5
Anthony <antho.tedde@gmail.com>
2017-12-01 19:43:06 +0100

    update the sstock function in order to get the same form as the one in page 106 (shreve ii)

--- R/sstock.R
+++ R/sstock.R
@@ -1,15 +1,24 @@
+#' Stock ticker simulation
+#'
+#' \code{sstock} outputs price path for a fully random stock evolution
+#'
+#'
 sstock <- function(initial_stock_price = 50,
                    time_to_maturity = 4,
                    seed = 1,
                    scale = 100,
-                   sigma = 1){
+                   sigma = 1,
+                   alpha = 0){
 
   # Create a Random Walk according to the params
   rw <- RandomWalk::srwalk(time_to_maturity = time_to_maturity,
                            seed = seed,
                            scale = scale)
 
-  X <- sigma * rw[, 2] - 0.5 * sigma ^ 2 * rw[, 1]
+  W <- rw$random_walk_path
+  t <- rw$time_periods
+
+  X <- sigma * W + ( alpha - 0.5 * sigma ^ 2 ) * t
 
   S <- initial_stock_price * exp(X)
   structure(data.frame(rw[, 1], S),
